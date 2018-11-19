3da477e330ffc178642c43dcefd1ea58a30ff06d
engdavide <twodoorbells@gmail.comgit config --global user.email twodoorbells@gmail.comgit config --global user.email twodoorbells@gmail.com>
2018-11-19 19:03:30 +0000

    add basic http routing

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
 
-var data = []
+var data = [
+    {name: "test data"}
 
+]
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
     res.render("landing", {data: data})
 });
 
-app.post("/", function(req, res))
+app.post("/new", function(req, res){
+    console.log("POST");
+    var newData = req.body;
+    data.push(newData);
+    res.redirect("/");
+});
 
 
 
