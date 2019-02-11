b96ef782c726a2f9143e59b95d18300df2fd8c38
engdavide <twodoorbells@gmail.comgit config --global user.email twodoorbells@gmail.comgit config --global user.email twodoorbells@gmail.com>
2019-02-11 13:50:07 +0000

    add gzip encoding

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
+const   zlib = require('zlib');
+const   request = require('request');
+var headers = {
+    'Accept-Encodring': 'gzip'
+};
+
 
 var data = [
     {entry: "test data"}
@@ -9,7 +15,7 @@ var data = [
 ]
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json())
+app.use(bodyParser.json()) //need this to pass in JSON post requests
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
@@ -18,6 +24,16 @@ app.get("/", function(req, res){
 
 app.get("/new", function(req,res){
     res.render("new");
+});
+
+app.post("/new", function(req, res){
+    console.log("POST");
+    console.log(req);
+    console.log(req.body);
+    var newEntry = req.body.entry;
+    var newData = {entry: newEntry};
+    data.push(newData);
+    res.redirect("/");
 });
 
 app.post("/new", function(req, res){
